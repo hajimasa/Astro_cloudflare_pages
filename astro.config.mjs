5657c07d2a21b0a3dfab8f835f576f8fe21e02bc
shoji_hajime <shoji_hajime@giginc.co.jp>
2023-04-18 17:59:06 +0900

    add: cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import sitemap from '@astrojs/sitemap';
 import compress from 'astro-compress';
 import purgecss from 'astro-purgecss';
 import { defineConfig } from 'astro/config';
+import cloudflare from "@astrojs/cloudflare";
 
+// https://astro.build/config
 export default defineConfig({
   // Your final, deployed URL
   site: 'https://www.my-site.dev',
   // The base path to deploy to
-  base: '/foo',
+  base: '/',
   integrations: [sitemap(), purgecss(), compress()],
+  output: "server",
+  adapter: cloudflare()
 });
