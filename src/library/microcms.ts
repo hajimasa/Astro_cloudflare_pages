e978b34019d32f4e834868a476e2e4d8e0044e1e
shojihajime <shojihajime@shojihajime.local>
2023-05-25 14:46:48 +0900

    fix: 環境変数が読めない問題を解決

--- src/library/microcms.ts
+++ src/library/microcms.ts
@@ -1,11 +1,22 @@
-//SDK利用準備
 import { createClient, MicroCMSQueries } from "microcms-js-sdk";
-const client = createClient({
-  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
-  apiKey: import.meta.env.MICROCMS_API_KEY,
-});
+// const client = createClient({
+//   serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+//   apiKey: import.meta.env.MICROCMS_API_KEY,
+// });
 
-//型定義
+// Cloudflare Pages SSR用関数
+import { getRuntime } from "@astrojs/cloudflare/runtime";
+const generateClient = (request: Request) => {
+  // runtimeの中にはenvの他、KVなど他のサービスなどがコンテキストとして入ってくる
+  const runtime = getRuntime(request);
+  //@ts-expect-error envの型は適宜調整
+  const { MICROCMS_SERVICE_DOMAIN, MICROCMS_API_KEY } = runtime.env;
+  return createClient({
+    serviceDomain: MICROCMS_SERVICE_DOMAIN,
+    apiKey: MICROCMS_API_KEY,
+  });
+};
+
 export type Blog = {
   id: string;
   createdAt: string;
@@ -14,6 +25,11 @@ export type Blog = {
   revisedAt: string;
   title: string;
   content: string;
+  eyecatch: {
+    url: string;
+    height: number;
+    width: number;
+  };
 };
 export type BlogResponse = {
   totalCount: number;
@@ -21,18 +37,38 @@ export type BlogResponse = {
   limit: number;
   contents: Blog[];
 };
+// class CMSBlog {
+//   @Cache(userCache, { ttl: 300 })
+export const getBlogs = async (request: Request, queries?: MicroCMSQueries) => {
+  // public async getBlogs(queries?: MicroCMSQueries){
+  // const client = clientFactoryFunction();
+  const client = generateClient(request);
+  const data = await client.get<BlogResponse>({ endpoint: "blogs", queries });
 
-//APIの呼び出し
-export const getBlogs = async (queries?: MicroCMSQueries) => {
-  return await client.get<BlogResponse>({ endpoint: "blogs", queries });
+  if (data.offset + data.limit < data.totalCount) {
+    queries ? (queries.offset = data.offset + data.limit) : "";
+    const result: BlogResponse = await getBlogs(request, queries);
+    return {
+      offset: result.offset,
+      limit: result.limit,
+      contents: [...data.contents, ...result.contents],
+      totalCount: result.totalCount,
+    };
+  }
+  return data;
 };
+
 export const getBlogDetail = async (
+  request: Request,
   contentId: string,
   queries?: MicroCMSQueries
 ) => {
+  const client = generateClient(request);
   return await client.getListDetail<Blog>({
     endpoint: "blogs",
     contentId,
     queries,
   });
 };
+// };
+// export const cmsBlog = new CMSBlog();
